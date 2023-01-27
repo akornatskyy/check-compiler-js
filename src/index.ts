045319ea21843b5c978ba8a925e89522506cd058
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2023-01-27 20:14:53 +0200

    feat: export compile with default builder.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import {DefaultBuilder} from './builder';
+import {Check, Compiler} from './compiler';
+import {Rule} from './types';
+
 export {Rule, Violation} from './types';
+
+const COMPILER = new Compiler(new DefaultBuilder([]));
+
+export function compile<T>(rule: Rule<T>): Check<T> {
+  return COMPILER.compile(rule);
+}
