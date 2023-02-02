bca720478366a560f13edd0f940c466123f817f6
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2023-02-02 20:14:19 +0200

    feat: introduced string rule.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import {DefaultBuilder} from './builder';
 import {Check, Compiler} from './compiler';
 import {numberRuleBuilder} from './rules/number';
+import {stringRuleBuilder} from './rules/string';
 import {Rule} from './types';
 
 export {Rule, Violation} from './types';
 
-const COMPILER = new Compiler(new DefaultBuilder([numberRuleBuilder]));
+const COMPILER = new Compiler(
+  new DefaultBuilder([numberRuleBuilder, stringRuleBuilder]),
+);
 
 export function compile<T>(rule: Rule<T>): Check<T> {
   return COMPILER.compile(rule);
---
+++ src/rules/string.ts
@@ -0,0 +1,119 @@
+import {Builder, RuleBuilder} from '../builder';
+import {Rule} from '../types';
+
+export function buildRuleString<T>(builder: Builder, rule: Rule<T>): string {
+  if (rule.type !== 'string') {
+    throw new Error(`Unexpected "${rule.type}" rule.`);
+  }
+
+  const {min, max, pattern, nullable} = rule;
+  const src: string[] = [];
+  if (!nullable) {
+    src.push(`
+    if (value === null) {
+      ${builder.addViolation({
+        reason: 'field not null',
+        message: 'Required field cannot be null.',
+      })}
+    }
+    else {`);
+  } else {
+    src.push(`
+    if (value !== undefined && value !== null) {`);
+  }
+
+  src.push(`
+      if (typeof value !== "string") {
+        ${builder.addViolation(
+          nullable
+            ? {
+                reason: 'string null',
+                message: 'Required to be a string or null.',
+              }
+            : {
+                reason: 'string',
+                message: 'Required to be a string.',
+              },
+        )}
+      }`);
+
+  if (min !== undefined) {
+    if (min < 0) {
+      throw new Error('Negative min string length.');
+    }
+
+    if (min > 0) {
+      src.push(`
+      else if (value.length === 0) {
+        ${builder.addViolation({
+          reason: 'string blank',
+          message: `Required field cannot be left blank.`,
+        })}
+      }`);
+    }
+
+    if (max !== undefined) {
+      if (min > max) {
+        throw new Error('String min length is greater than max.');
+      }
+
+      if (min === max) {
+        src.push(`
+      else if (value.length !== ${max}) {
+        ${builder.addViolation({
+          reason: 'string exact',
+          message: `The length must be exactly ${max} characters.`,
+        })}
+      }`);
+      } else {
+        src.push(`
+      else if (value.length < ${min} || value.length > ${max}) {
+        ${builder.addViolation({
+          reason: 'string range',
+          message: `Required to be between ${min} and ${max} characters in length.`,
+        })}
+      }`);
+      }
+    } else {
+      src.push(`
+      else if (value.length < ${min}) {
+        ${builder.addViolation({
+          reason: 'string min',
+          message: `Required to be a minimum of ${min} characters in length.`,
+        })}
+      }`);
+    }
+  } else if (max !== undefined) {
+    if (max < 0) {
+      throw new Error('Negative max string length.');
+    }
+
+    src.push(`
+      else if (value.length > ${max}) {
+        ${builder.addViolation({
+          reason: 'string max',
+          message: `Exceeds maximum length of ${max} characters.`,
+        })}
+      }`);
+  }
+
+  if (pattern) {
+    src.push(`
+      else if (!${pattern}.test(value)) {
+        ${builder.addViolation({
+          reason: 'string pattern',
+          message: `Required to match validation pattern.`,
+        })}
+      }`);
+  }
+
+  src.push(`
+    }`);
+
+  return src.join('');
+}
+
+export const stringRuleBuilder: RuleBuilder = {
+  types: ['string'],
+  build: buildRuleString,
+};
