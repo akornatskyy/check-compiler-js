99b6504ecf98ef6da783c598cd65e5c96f842b62
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2023-02-05 20:41:55 +0200

    feat: introduced object rule.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import {DefaultBuilder} from './builder';
 import {Check, Compiler} from './compiler';
 import {numberRuleBuilder} from './rules/number';
+import {objectRuleBuilder} from './rules/object';
 import {stringRuleBuilder} from './rules/string';
 import {Rule} from './types';
 
 export {Rule, Violation} from './types';
 
 const COMPILER = new Compiler(
-  new DefaultBuilder([numberRuleBuilder, stringRuleBuilder]),
+  new DefaultBuilder([numberRuleBuilder, stringRuleBuilder, objectRuleBuilder]),
 );
 
 export function compile<T>(rule: Rule<T>): Check<T> {
---
+++ src/rules/object.ts
@@ -0,0 +1,96 @@
+import {Builder, RuleBuilder} from '../builder';
+import {Rule} from '../types';
+
+export function buildRuleObject<T>(builder: Builder, rule: Rule<T>): string {
+  if (rule.type !== 'object') {
+    throw new Error(`Unexpected "${rule.type}" rule.`);
+  }
+
+  const {properties, required, nullable} = rule;
+  const src: string[] = [];
+  if (!nullable) {
+    src.push(`
+    if (value === null) {
+      ${builder.addViolation({
+        reason: 'object not null',
+        message: 'Required object cannot be null.',
+      })}
+    }
+    else {`);
+  } else {
+    src.push(`
+    if (value !== undefined && value !== null) {`);
+  }
+
+  src.push(`
+      const __o = value;
+      if (typeof value !== "object") {
+        ${builder.addViolation(
+          nullable
+            ? {
+                reason: 'object null',
+                message: 'Required to be an object or null.',
+              }
+            : {
+                reason: 'object',
+                message: 'Required to be an object.',
+              },
+        )}
+      }
+      else {`);
+
+  src.push(`
+        const {${Object.keys(properties).join(', ')}} = __o;`);
+
+  if (required) {
+    src.push(`
+        let ok = true;`);
+    for (const r of required) {
+      const name = String(r);
+      src.push(`
+        if (${name} === undefined || ${name} === null) {
+          ok = false;
+          ${builder.addViolation({
+            location: name,
+            reason: 'required',
+            message: 'Required field cannot be left blank.',
+          })}
+        }
+        `);
+    }
+
+    src.push(`
+        if (ok) {`);
+  }
+
+  for (const [name, value] of Object.entries(properties)) {
+    const inner = builder.build(value as Rule<unknown>, name);
+    if (inner) {
+      src.push(`
+  { /* ${name} */`);
+      if (name !== 'value') {
+        src.push(`
+    const value = ${name};`);
+      }
+
+      src.push(`${inner}
+  } /* ${name} */`);
+    }
+  }
+
+  if (required) {
+    src.push(`
+        } /* if ok */`);
+  }
+
+  src.push(`
+      }
+    }`);
+
+  return src.join('');
+}
+
+export const objectRuleBuilder: RuleBuilder = {
+  types: ['object'],
+  build: buildRuleObject,
+};
